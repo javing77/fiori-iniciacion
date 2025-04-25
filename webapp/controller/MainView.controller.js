508b98bc892c47ced51ee51767426a8c3258ff03
Javing77 <javing77@gmail.com>
2025-04-25 20:41:12 +0000

    End 5

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,24 +1,50 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], 
+/**
+ * 
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ * @param {typeof sap.ui.model.Filter} Filter
+ * @param {typeof sap.ui.model.FilterOperator} FilterOperator
+ */
+(Controller,Filter,FilterOperator) => {
     "use strict";
 
     return Controller.extend("logaligroup.invoices.controller.MainView", {
-        onInit() {
-            const oJSONMolde = new sap.ui.model.json.JSONModel();
+        onInit: function() {
+            const oJSONModel = new sap.ui.model.json.JSONModel();
             const oView = this.getView();
-            oJSONMolde.loadData("./model/SelectionScreenMenu.json")
-            oView.setModel(oJSONMolde, "selectionScreen" )
+            oJSONModel.loadData("./model/SelectionScreenMenu.json");
+            oView.setModel(oJSONModel, "selectionScreen");
         },
 
-        onFilter(oEvent) {
+        onFilter: function (oEvent){
+            const oData = this.getView().getModel("selectionScreen").getData();
+            let filters = [];
 
+            if(oData.ShipName != ""){
+                filters.push(new Filter("ShipName", FilterOperator.Contains, oData.ShipName));
+            }
+
+            if(oData.Countrykey != ""){
+                filters.push(new Filter("Country", FilterOperator.EQ, oData.Countrykey));
+            }
+
+            const oList = this.getView().byId("invoicesList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
         },
 
-        onClearFilter(oEvent){
-            const oModelSeScreen = this.getView().getModel("selectionScreen");
-            oModelSeScreen.setProperty("/CountryKey", "")
-            oModelSeScreen.setProperty("/ShipName", "")
+        onClearFilter: function (){
+            const oModelSelScreen = this.getView().getModel("selectionScreen");
+            oModelSelScreen.setProperty("/ShipName", "");
+            oModelSelScreen.setProperty("/Countrykey", "");
+
+            const oList = this.getView().byId("invoicesList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter([]);
         }
     });
 });
